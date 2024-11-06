65d9a6bd3800b345a18b34e19a1a75a2ac365be0
Ann Root <ann.root@bvsd.org>
2024-11-06 13:54:25 -0700

    Update cavegame.py

--- cavegame.py
+++ cavegame.py
@@ -1,1 +1,5 @@
-
+room_list = []
+room = ["South Hall", 4, 2, None, 0]  # from south hall go north to room 4, east to room 2 and west to room 0
+room_list.append(room)
+room = ["Dining Room", 5, None, None, 1] #  create another room
+room_list.append(room)
